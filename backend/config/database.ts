364dfdd781cb92de1df0dcc06d0bdeb64e9f0ae7
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2022-09-13 08:54:49 -0600

    remove dist folders

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'postgres'),
-      username: env('DATABASE_USERNAME', 'jarrodmedrano'),
+      user: env('DATABASE_USERNAME', 'jamedrano'),
       password: env('DATABASE_PASSWORD', 'postgres'),
       schema: env('DATABASE_SCHEMA', 'public'),
     },
---
+++ backend/dist/config/database.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+    defaultConnection: 'default',
+    connection: {
+        client: 'postgres',
+        connection: {
+            host: env('DATABASE_HOST', 'localhost'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'postgres'),
+            user: env('DATABASE_USERNAME', 'jamedrano'),
+            password: env('DATABASE_PASSWORD', 'postgres'),
+            schema: env('DATABASE_SCHEMA', 'public'),
+        },
+    },
+});
