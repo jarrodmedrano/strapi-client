0e089aa3a92b91a6cec714e26395d2e40594b031
Jarrod Medrano <jarrod.medrano@changehealthcare.com>
2022-09-13 09:31:18 -0600

    fix backend

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,14 +1,13 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', 'jamedrano'),
       password: env('DATABASE_PASSWORD', 'postgres'),
-      schema: env('DATABASE_SCHEMA', 'public'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
