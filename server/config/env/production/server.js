0a8a41cac3e98104793e31747973e7191c207abf
Jarrod Medrano <jmedran@gmail.com>
2022-09-26 07:12:09 -0600

    change port to hardcoded

--- server/config/env/production/server.js
+++ server/config/env/production/server.js
@@ -1,9 +1,8 @@
 // path: ./config/env/production/server.ts
-
 export default ({ env }) => ({
   proxy: true,
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: 1337,
   // url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
   app: {
     keys: env.array('APP_KEYS'),
