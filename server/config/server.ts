e87ab2634f8bef76e7a82590d882b86e7a0599ad
Jarrod Medrano <jmedran@gmail.com>
2022-09-26 18:47:23 -0600

    add app keys

--- server/config/server.ts
+++ server/config/server.ts
@@ -2,6 +2,9 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('STRAPI_PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [
+      'Sl/3T8adNMB5C44t+7UQ9Q==',
+      'gAogfl8DZ5pDQDLxFIMlXw==,NPOlZB+Kln2eASlpj6K6bQ==,auFqhbrYTJJl/8YiDoJ4Fw==',
+    ],
   },
 });
