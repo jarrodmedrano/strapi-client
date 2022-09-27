5d8e172790394aef9001471b2e6b8ccb628a3188
Jarrod Medrano <jmedran@gmail.com>
2022-09-26 19:10:31 -0600

    change docker compose

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6e5aea8dc643807984c7fef458c9ed3a'),
+    },
+  },
+});
