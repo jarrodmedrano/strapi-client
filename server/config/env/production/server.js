fff155e56a48298c7a19cd62c9563b7c64116d60
Jarrod Medrano <jmedran@gmail.com>
2022-09-26 18:59:01 -0600

    force set keys

--- server/config/env/production/server.js
+++ server/config/env/production/server.js
@@ -6,6 +6,9 @@ export default ({ env }) => ({
   port: env.int('STRAPI_PORT', 1337),
   // url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      'Sl/3T8adNMB5C44t+7UQ9Q==,gAogfl8DZ5pDQDLxFIMlXw==,NPOlZB+Kln2eASlpj6K6bQ==',
+      'auFqhbrYTJJl/8YiDoJ4Fw==',
+    ]),
   },
 });
